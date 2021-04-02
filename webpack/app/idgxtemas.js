ccd54dec0a1811c96e2d98f53c35538716d0f9e5
Cleber J Santos <cleber@cleberjsantos.com.br>
2021-04-02 08:19:02 -0300

    Remove social like references

--- webpack/app/idgxtemas.js
+++ webpack/app/idgxtemas.js
@@ -1,16 +1,11 @@
 import Contraste from './js/contrast.js';
-import SocialLike from './js/sociallike';
 import Navegacao from './js/navegacao.js';
 
 
 $(() => {
-  if ($('#viewlet-social-like')[0] != null) {
-    new SocialLike();
-  }
 });
 
 export default {
   Contraste,
-  SocialLike,
   Navegacao,
 }
--- webpack/app/js/navegacao.js
+++ webpack/app/js/navegacao.js
@@ -1,34 +1,27 @@
-
 jQuery(function($) {
     $(".icon-menu").click(function(){
         $("#mainnavigation-wrapper").toggleClass("menu-aberto");
     });
-});
-jQuery(function($) {
+
     $(".icon-menu-aberto").click(function(){
         $("#mainnavigation-wrapper").toggleClass("menu-aberto");
     });
-});
+  
+    $( "#portal-globalnav li:has(.submenu)" ).addClass( "tem-submenu" );
 
-jQuery(function($) {
-  $( "#portal-globalnav li:has(.submenu)" ).addClass( "tem-submenu" );
-});
-
-jQuery(function($) {
     $("#portal-searchbox .searchField").focus(function(){
         $("#portal-searchbox").toggleClass("busca-ativada");
     });
+
     $("#portal-searchbox .searchField").blur(function(){
         $("#portal-searchbox").toggleClass("busca-ativada");
     });
-});
 
-jQuery(function($) {
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 60) {
-      $("body").addClass("header-fixo");
-    } else {
-      $("body").removeClass("header-fixo");
-    }
-  });
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 60) {
+        $("body").addClass("header-fixo");
+      } else {
+        $("body").removeClass("header-fixo");
+      }
+    });
 });
